d0ef89a9e8b9c51f8e30f9d30e3e275726557894
halleluya2013idone <halleluya2013idone@gmail.com>
2024-09-06 23:13:13 +0000

    feat: completed assessment endpoints

--- server/src/routes/assessmentRoutes.ts
+++ server/src/routes/assessmentRoutes.ts
@@ -1,9 +1,10 @@
 import express from 'express'
-import { createAssessment } from '../controllers/AssessmentControllers'
+import { createAssessment, evaluateAssessment } from '../controllers/AssessmentControllers'
 import { authenticatToken } from '../middlewares/authToken'
 
 const router = express.Router()
 
 router.post('/create', authenticatToken, createAssessment)
+router.post('/analyze/:id', authenticatToken, evaluateAssessment)
 
 export = router
